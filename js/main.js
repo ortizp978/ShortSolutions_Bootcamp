28dd04fd8f1cc17ddcb20e79ca7c77a03d3ec087
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-09-09 11:30:09 -0400

    other changes

--- js/main.js
+++ js/main.js
@@ -7,11 +7,16 @@
   let currentHouse = '';
 
     const teamText = [
-      `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum venenatis purus, sit amet vestibulum nisl ultrices dapibus. Aliquam dictum elementum blandit. Curabitur auctor nisl ut lorem luctus, elementum feugiat augue ullamcorper. Nam et pharetra libero. Sed vel tortor mi. Donec sit amet condimentum arcu, eleifend interdum purus. Cras feugiat mauris a magna porta, vel viverra mauris aliquam. Donec semper dolor libero, sed bibendum lacus euismod convallis. Donec sed pretium purus. Mauris vel nunc nec diam porttitor mattis.`,
-
       `Nam tincidunt, arcu ac accumsan bibendum, quam tortor tincidunt lectus, vel lobortis leo mauris at ligula. Aliquam et ligula cursus, lobortis purus eget, iaculis nunc. In quis ante eget mauris molestie mattis non vel lectus. Praesent ullamcorper eleifend tellus in auctor. Aenean pretium elementum fringilla. Suspendisse eget pretium lorem, nec maximus risus. Morbi convallis blandit sem a imperdiet. Nam et leo dolor. Nunc mattis, nibh id interdum porta, nunc turpis rhoncus arcu, non convallis nisi arcu vitae velit. Aenean aliquet, lorem condimentum lacinia rhoncus, nisi metus auctor risus, in luctus urna dolor id nunc. Nam eu tristique eros. Nullam tristique mollis tempor. Duis a commodo leo, sit amet elementum ipsum. Sed consectetur eu purus gravida bibendum.`,
 
-      `Ut imperdiet nibh eu quam euismod, sit amet cursus urna tempus. Aenean posuere arcu viverra, aliquet neque a, ultricies magna. Nunc dignissim vestibulum lectus vel iaculis. Vestibulum nisi erat, rhoncus sit amet nibh a, fringilla congue lorem. Nam in tortor nibh. Sed in elit finibus, hendrerit mi at, imperdiet velit. Donec molestie tincidunt congue. Integer iaculis, diam nec maximus malesuada, lorem orci dignissim nisl, nec semper nulla mauris quis neque. Mauris justo metus, consequat sit amet risus vitae, gravida luctus nunc.`,
+      `My name is Amr, and Student at Fanshawe college. I started an interactive media design program in 2022.
+      I have more than six years of experience in graphic design. Specifically. Advertising and branding.
+      My artwork was published at the Caan film festival and Berlin film festival when I was working as a senior graphic designer in Dubai. I created the whole campaign independently
+      I Developed concepts to meet company needs: collaborated with clients regarding corporate branding and ensured consistency throughout campaigns and marketing tools.
+      I Developed and designed branding for companies including business logo design, stationary requirements, social media, and Marketing presentations.`,
+
+      `Professional in marketing and advertising with an emphasis on graphic design, branding and team leadership with more than 20 years of experience; I have worked in the cosmetics and personal care industry from my field of design and communication.
+      "Drawing by hand first makes everything look better in the end"; I am a lover of practical and direct design above the bombastic; saturating does not communicate more because less is more.`,
 
       `Quisque ut sagittis enim. Suspendisse potenti. Nulla consectetur augue a augue imperdiet viverra. Nam et arcu lectus. Cras venenatis, tellus in convallis vulputate, ipsum diam pellentesque ex, sit amet lobortis augue lacus a sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Maecenas consectetur vitae justo nec lacinia.`,
     ]
